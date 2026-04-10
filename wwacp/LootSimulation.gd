78cd068c1aeb1f4f9070031664cf86486e6bdddf
Glacier94 <etrommer1@gmail.com>
2026-04-10 18:49:36 +0200

    finalized optional mag drop

--- wwacp/LootSimulation.gd
+++ wwacp/LootSimulation.gd
@@ -4,7 +4,17 @@ const SPAWN_MAG = 1
 
 # After the vanilla SpawnItems runs, spawn ammo and magazine for every weapon that was placed.
 func SpawnItems():
+	var children_before = get_children()
 	super()
+
+	# Reposition weapon pickups added by super so they stay within 1m of our origin
+	for child in get_children():
+		if child in children_before:
+			continue
+		if child is RigidBody3D:
+			child.position = Vector3(randf_range(-1.0, 1.0), 0.1, randf_range(-1.0, 1.0))
+			child.linear_velocity = Vector3.ZERO
+
 	for itemData in loot:
 		if itemData.type != "Weapon":
 			continue
@@ -20,9 +30,9 @@ func SpawnItems():
 			continue
 		var ammoPickup = ammoScene.instantiate()
 		add_child(ammoPickup)
-		var dropDir = Vector3(randf_range(-1.0, 1.0), randf_range(-1.0, 1.0), randf_range(-1.0, 1.0))
+		ammoPickup.position = Vector3(randf_range(-1.0, 1.0), 0.1, randf_range(-1.0, 1.0))
 		ammoPickup.Unfreeze()
-		ammoPickup.linear_velocity = dropDir * 10.0
+		ammoPickup.linear_velocity = Vector3.ZERO
 		var newSlotData = SlotData.new()
 		newSlotData.itemData = ammoItem
 		if ammoItem.defaultAmount != 0:
@@ -43,9 +53,9 @@ func SpawnItems():
 					break
 				var magPickup = magScene.instantiate()
 				add_child(magPickup)
-				var magDropDir = Vector3(randf_range(-1.0, 1.0), randf_range(-1.0, 1.0), randf_range(-1.0, 1.0))
+				magPickup.position = Vector3(randf_range(-1.0, 1.0), 0.1, randf_range(-1.0, 1.0))
 				magPickup.Unfreeze()
-				magPickup.linear_velocity = magDropDir * 10.0
+				magPickup.linear_velocity = Vector3.ZERO
 				var magSlotData = SlotData.new()
 				magSlotData.itemData = compatible_item
 				magPickup.slotData = magSlotData
