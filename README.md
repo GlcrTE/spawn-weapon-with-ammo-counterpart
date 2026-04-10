f9c601225f487e2174a6e85b841414b27436d1d2
GlcrTE <Etrommer1@gmail.com>
2026-04-10 22:55:38 +0200

    Mag opt (#1)
    * Added the option to add an magazin suitable for the weapon spawned.
    
    * not needed
    
    * finalized optional mag drop
    
    * deleted unneeded file
    
    * Created version 0.2.0
    
    * Update README.md
    
    * fixed version

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Spawn Weapon With Ammo Counterpart
-This mod is a simple QoL improvement that lets every weapon in the game spawn with it's counterpart.
+This mod is a simple QoL improvement that lets every weapon in the game spawn with it's ammo/mag counterpart.
 
 Easily install with moving the .vmz to your mods folder.
 MCM not included yet. Maybe in the future, but not necessary imo.
--- wwacp/LootSimulation.gd
+++ wwacp/LootSimulation.gd
@@ -1,8 +1,20 @@
 extends "res://Scripts/LootSimulation.gd"
 
-# After the vanilla SpawnItems runs, spawn ammo for every weapon that was placed.
+const SPAWN_MAG = 1
+
+# After the vanilla SpawnItems runs, spawn ammo and magazine for every weapon that was placed.
 func SpawnItems():
+	var children_before = get_children()
 	super()
+
+	# Reposition weapon pickups added by super so they stay within 1m of our origin
+	for child in get_children():
+		if child in children_before:
+			continue
+		if child is RigidBody3D:
+			child.position = Vector3(randf_range(-1.0, 1.0), 0.1, randf_range(-1.0, 1.0))
+			child.linear_velocity = Vector3.ZERO
+
 	for itemData in loot:
 		if itemData.type != "Weapon":
 			continue
@@ -18,9 +30,9 @@ func SpawnItems():
 			continue
 		var ammoPickup = ammoScene.instantiate()
 		add_child(ammoPickup)
-		var dropDir = Vector3(randf_range(-1.0, 1.0), randf_range(-1.0, 1.0), randf_range(-1.0, 1.0))
+		ammoPickup.position = Vector3(randf_range(-1.0, 1.0), 0.1, randf_range(-1.0, 1.0))
 		ammoPickup.Unfreeze()
-		ammoPickup.linear_velocity = dropDir * 10.0
+		ammoPickup.linear_velocity = Vector3.ZERO
 		var newSlotData = SlotData.new()
 		newSlotData.itemData = ammoItem
 		if ammoItem.defaultAmount != 0:
@@ -29,3 +41,22 @@ func SpawnItems():
 			if randi_range(0, 100) < 10:
 				newSlotData.state = "Frozen"
 		ammoPickup.slotData = newSlotData
+		if SPAWN_MAG == 0:
+			continue
+		for compatible_item in weaponData.compatible:
+			if compatible_item.subtype == "Magazine":
+				if compatible_item.file == "":
+					break
+				var magScene = Database.get(compatible_item.file)
+				if magScene == null:
+					print("[WeaponAmmoSpawner] Magazine scene not found: " + compatible_item.file)
+					break
+				var magPickup = magScene.instantiate()
+				add_child(magPickup)
+				magPickup.position = Vector3(randf_range(-1.0, 1.0), 0.1, randf_range(-1.0, 1.0))
+				magPickup.Unfreeze()
+				magPickup.linear_velocity = Vector3.ZERO
+				var magSlotData = SlotData.new()
+				magSlotData.itemData = compatible_item
+				magPickup.slotData = magSlotData
+				break
